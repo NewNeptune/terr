dc21f92581318d23078a4f42c4a320177d13e484
NewNeptune <100222801+NewNeptune@users.noreply.github.com>
2022-03-29 17:06:41 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  profile    = "default"
+  region     = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-074251216af698218"
+  instance_type = "t2.micro"
+
+}
